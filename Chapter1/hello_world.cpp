320c31b714bd08ab0c725162870e448999e4b2c4
Jeff-Meendering <jsmeendering@gmail.com>
2023-10-19 23:16:25 -0400

    feat: add the std_lib_facilities.h update: hello_world.cpp

--- Chapter1/hello_world.cpp
+++ Chapter1/hello_world.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-using namespace std;
-inline void keep_window_open() { char ch; cin>>ch;}
+#include "../std_lib_facilities.h"
 
 int main() {
     cout << "Hello, World!\n";
