a7a8bf6aeeb55f0ab2332b62c36d8a70aa72872f
Jeff-Meendering <112517844+Jeff-Meendering@users.noreply.github.com>
2024-01-08 23:59:50 -0500

    try_this_2 added

---
+++ Chapter3/try_this_2.cpp
@@ -0,0 +1,11 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+    cout << "Please enter your name and then your age (in years)...";
+    string name;
+    int age;
+    return 0;
+    int age_months = age * 12;
+    cin >> name >> age;
+    cout << "Hello, " << name << " your age in months is " << age_months << ".";
+}
